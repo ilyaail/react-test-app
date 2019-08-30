af5a5b465f4d0631da35cba6bf7570e44796b9fd
Ilya <ilyailysha.k@gmail.com>
2019-08-30 18:05:56 +0300

    s

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './man.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo-2" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
